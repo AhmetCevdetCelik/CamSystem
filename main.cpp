5e72762fd4ba312ff561b877d37ea9ebd16dc2c9
AhmetCevdetCelik <ahmetcevdet006@gmail.com>
2026-04-22 18:07:17 +0300

    Final cleanup for camera system

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 int main() {
     // Creating an object of Camera class
     Camera trafficCam("192.168.1.50", 60);
+    Camera cam2("192.168.1.2", 60);
 
     trafficCam.Connect();
     trafficCam.CaptureStream();
