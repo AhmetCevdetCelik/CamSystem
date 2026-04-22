9329e861f4bfafc6aed2917e49e39f274f42e91a
AhmetCevdetCelik <ahmetcevdet006@gmail.com>
2026-04-22 18:22:01 +0300

    fact:add member

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 
 int main() {
     // Creating an object of Camera class
-    Camera trafficCam("192.168.1.50", 60);
-    Camera trafficCam("192.168.1.2", 60);
+    Camera trafficCam1("192.168.1.50", 60);
+    Camera trafficCam2("192.168.1.2", 60);
 
-    trafficCam.Connect();
-    trafficCam.CaptureStream();
+    trafficCam1.Connect();
+    trafficCam1.CaptureStream();
+    trafficCam2.Connect();
+    trafficCam2.CaptureStream();
 
     return 0;
 }
